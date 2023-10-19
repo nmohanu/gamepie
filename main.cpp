08eb289f9cce9235cf3a3a8230a7f551ce475bd6
nmohanu <nmohanu@gmail.com>
2023-10-19 23:34:36 +0200

    added player movement and delta time

--- main.cpp
+++ main.cpp
@@ -4,34 +4,60 @@
 int main()
 {
     // Initialize
-    float moveSpeed = 0.1;
+    float moveSpeed;
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Game");
+    sf::RenderWindow window(sf::VideoMode(1920/2, 1080/2), "Game", sf::Style::Default, settings);
+    window.setFramerateLimit(60);
+    float scaler = 4.f;
 
-        sf::Texture playerTexture;
-        sf::Sprite playerSprite;
 
-        if (playerTexture.loadFromFile("../Assets/Player/Textures/player.gif"))
-        {
-            playerSprite.setTexture(playerTexture);
-            std::cout << "texture loaded \n";
-        }
-        else
-        {
-            std::cout << "texture failed to load \n";
-        }
 
-        int XIndex = 0;
-        int YIndex = 0;
+    // Fps count
+    sf::Clock clock;
+    sf::Clock timeFPS;
 
-        //playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
-        //playerSprite.scale(sf::Vector2f(3, 3));
+    // Player
+    sf::Clock playerClock;
+    bool justMoved= false;
+    bool left = false;
+
+    sf::Texture player_texture_right;
+    player_texture_right.loadFromFile("../Assets/Player/Textures/player_r.png");
+
+    sf::Texture player_texture_left;
+    player_texture_left.loadFromFile("../Assets/Player/Textures/player_l.png");
+
+    sf::Texture player_texture_mright;
+    player_texture_mright.loadFromFile("../Assets/Player/Textures/player_mr.png");
+
+    sf::Texture player_texture_mleft;
+    player_texture_mleft.loadFromFile("../Assets/Player/Textures/player_ml.png");
+
+    sf::Texture player_texture_front;
+    player_texture_front.loadFromFile("../Assets/Player/Textures/player_f.png");
+
+    sf::Sprite playerSprite;
+    playerSprite.setTexture(player_texture_right);
+
+
+
+    playerSprite.setScale(scaler, scaler);
 
 
     // Game loop
     while (window.isOpen())
     {
+
+        // Compute frame rate
+        float deltaTime = clock.restart().asSeconds();
+        moveSpeed = 10.f * deltaTime * 60;
+        sf::Time elapsedFPS = timeFPS.getElapsedTime();
+        if(elapsedFPS.asSeconds() > 5){
+            std::cout << 1.0 / deltaTime << std::endl;
+            timeFPS.restart();
+        }
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -43,21 +69,39 @@ int main()
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
             sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(moveSpeed, 0));
+            playerSprite.setTexture(player_texture_mright);
+            justMoved = true;
+            playerClock.restart();
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
             sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(-moveSpeed, 0));
+            playerSprite.setTexture(player_texture_mleft);
+            left = true;
+            justMoved = true;
+            playerClock.restart();
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
             sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(0, -moveSpeed));
+            playerClock.restart();
         }
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
             sf::Vector2f position = playerSprite.getPosition();
             playerSprite.setPosition(position + sf::Vector2f(0, moveSpeed));
+            playerSprite.setTexture(player_texture_front);
+            playerClock.restart();
         }
-
-
+        sf::Time elapsed = playerClock.getElapsedTime();
+        if(elapsed.asMilliseconds() > 100 && justMoved){
+            if(left){
+                playerSprite.setTexture(player_texture_left);
+            } else {
+                playerSprite.setTexture(player_texture_right);
+            }
+            justMoved = false;
+            left = false;
+        }
         window.clear();
         window.draw(playerSprite);
         window.display();
